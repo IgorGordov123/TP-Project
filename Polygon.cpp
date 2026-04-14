5efd399183a78fe65655ca10a9cabaee76dd77d5
IgorGordov123 <igor@gordov.ru>
2026-04-14 22:43:38 +0300

    Area of a regular polygon

---
+++ Polygon.cpp
@@ -0,0 +1,16 @@
+﻿#include "Polygon.h"
+
+Polygon::Polygon(const std::vector<double>& s) : sides(s) {}
+
+double Polygon::getArea() const {
+    int n = sides.size();
+    if (n < 3) return 0;
+
+    double perimeter = 0;
+    for (double side : sides) {
+        perimeter += side;
+    }
+    double s = perimeter / n;
+    double apothem = s / (2 * tan(3.1415926 / n));
+    return (n * s * apothem) / 2;
+}
