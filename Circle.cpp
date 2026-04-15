5ba26dd607b69d83eee2496e2ca7572ecf933536
fedor1132 <fedor16022011@gmail.com>
2026-04-15 09:07:48 +0300

    delfiles

--- Circle.cpp
+++
@@ -1,6 +0,0 @@
-#include "Circle.h"
-
-Circle::Circle(double r) : radius(r) {}
-double Circle::getArea() const {
-    return 3.14 * radius * radius;
-}
--- IFigure2D.h
+++
@@ -1,5 +0,0 @@
-﻿class IFigure2D {
-public:
-    virtual double getArea() const = 0;
-    virtual ~IFigure2D() {}
-};
