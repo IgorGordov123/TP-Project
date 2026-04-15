57dd3f14be3a1d6a937a39118c26a26b125d53a1
fedor1132 <fedor16022011@gmail.com>
2026-04-15 08:51:59 +0300

    add explanation

--- Trapezoid.h
+++
@@ -1,9 +0,0 @@
-﻿#include "IFigure2D.h"
-
-class Trapezoid : public IFigure2D {
-private:
-    double base1, base2, height;
-public:
-    Trapezoid(double b1, double b2, double h);
-    double getArea() const override;
-};
